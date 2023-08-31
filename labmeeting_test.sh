ce28a7de4d4f786b4a7485515908ff2fb23a4e7c
sinhaeti <sinhaeti@yahoo.com>
2023-08-31 10:54:33 -0700

    add SBATCH header

--- labmeeting_test.sh
+++ labmeeting_test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "test!"
+#SBATCH --job-name=Test_Script
+#SBATCH --partition=panda_physbio
+#SBATCH --begin=now
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=4G
+#SBATCH --time=1-00:00:00
+#SBATCH --exclude=node002.panda.pbtech,node007.panda.pbtech,node010.panda.pbtech
 
+echo "test!"  > test.txt
