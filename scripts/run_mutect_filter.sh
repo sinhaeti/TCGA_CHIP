653894d2a7d6fb9718076104c595274214609677
sinhaeti <sinhaeti@yahoo.com>
2023-09-20 23:48:06 -0400

    remove VCF_PATHS_LIST, not used

--- scripts/run_mutect_filter.sh
+++ scripts/run_mutect_filter.sh
@@ -15,12 +15,12 @@ else
     #INPUT_VCF_DIR="/athena/elementolab/scratch/es984/TCGA_CHIP/TCGA_BAMS/renamed_vcfs/${TISSUE}/"
     OUTPUT_DIR="/athena/elementolab/scratch/es984/TCGA_CHIP/TCGA_FILTERED_CALLS/${TISSUE}/"
     
-    VCF_PATHS="${SCRIPT_DIR}/TCGA_CHIP_${TISSUE}_samples.txt" #give a path to a file to store the paths to the fastq files in $fastq_directory
+    #VCF_PATHS="${SCRIPT_DIR}/TCGA_CHIP_${TISSUE}_samples.txt" #give a path to a file to store the paths to the fastq files in $fastq_directory
 
-    ls "${INPUT_VCF_DIR}/"* | grep ".vcf" | sort -u > ${VCF_PATHS} #generate list of full paths to fastq files and save to the file in $fastq_list
+    #ls "${INPUT_VCF_DIR}/"* | grep ".vcf" | sort -u > ${VCF_PATHS} #generate list of full paths to fastq files and save to the file in $fastq_list
 
     ####### ALIGNMENT AND VARIANT CALLING  #########
-    VCF_PATHS_LIST=$(cat $VCF_PATHS)
+    #VCF_PATHS_LIST=$(cat $VCF_PATHS)
     for SAMPLE_SYM_PATH in ${INPUT_VCF_DIR}/*vcf
     do
         if [ ! -f "${OUTPUT_DIR}/${SAMPLE_PREFIX}_mutect2_filter_funcotator_coding.vcf" ]; then 
